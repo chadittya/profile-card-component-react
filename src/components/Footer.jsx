00b972283c520b26cea0cc8ca6d66d12ab8e50dd
chadittya <chadittya@gmail.com>
2023-01-24 08:15:15 +0700

    finishing project and deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="text-center">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="font-bold"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.frontendmentor.io/profile/chadittya"
+        className="font-bold"
+        target="_blank"
+      >
+        Galih Arizza
+      </a>
+      .
+    </div>
+  );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,41 +1,57 @@
 import React from "react";
+import avatarVictor from "../assets/images/image-victor.jpg";
 
 export default function ProfileCard() {
   return (
-    <div className="profile-card">
+    <div className="profile-card text-center">
       {/* container */}
       <div className="h-screen flex justify-center items-center">
         {/* card */}
-        <div className="bg-white rounded-xl box-border overflow-hidden">
+        <div className="relative bg-white rounded-xl box-border overflow-hidden md:w-[350px] w-[327px] md:h-[375px] h-[376px] flex flex-col">
           {/* 1 hero pattern */}
-          <div></div>
+          <div className="bg-[url(../assets/images/bg-pattern-card.svg)] w-full h-[140px] bg-no-repeat flex flex-initial"></div>
           {/* avatar */}
-          <div></div>
-          {/* 2 profile content */}
-          <div>
-            {/* profile content container */}
-            <div>
-              <h1>
-                name <span>age</span>
-              </h1>
-            </div>
-            <div>
-              <span>address</span>
-            </div>
+          <div className="flex absolute w-full justify-center mt-[89px]">
+            <img
+              src={avatarVictor}
+              alt="victor"
+              className="rounded-full border-[5px] border-white"
+            />
           </div>
-          {/* 3 stats  */}
-          <div>
-            <div>
-              <h2>stats</h2>
-              <span>f</span>
+          {/* 2 profile content */}
+          <div className="divide-y flex flex-auto flex-col relative mt-[70px] gap-4 box-border">
+            {/* profile content container */}
+            {/* profile content top */}
+            <div className="capitalize">
+              <div>
+                <h1 className="font-bold">
+                  victor crest{" "}
+                  <span className="font-normal text-c-darkGrayishBlue">26</span>
+                </h1>
+              </div>
+              <div>
+                <span className="text-sm text-c-darkGrayishBlue">london</span>
+              </div>
             </div>
+            {/* profile content bottom */}
             <div>
-              <h2>stats</h2>
-              <span>l</span>
-            </div>
-            <div>
-              <h2>stats</h2>
-              <span>p</span>
+              {/* 3 stats  */}
+              <div className="flex flex-row justify-between capitalize px-10 py-5">
+                <div>
+                  <h2 className="uppercase font-bold">80k</h2>
+                  <span className="text-sm text-c-darkGrayishBlue">
+                    followers
+                  </span>
+                </div>
+                <div>
+                  <h2 className="uppercase font-bold">803k</h2>
+                  <span className="text-sm text-c-darkGrayishBlue">likes</span>
+                </div>
+                <div>
+                  <h2 className="uppercase font-bold">1.4k</h2>
+                  <span className="text-sm text-c-darkGrayishBlue">photos</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
